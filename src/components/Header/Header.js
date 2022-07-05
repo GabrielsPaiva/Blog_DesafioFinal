bfcc101988f6fd66d3b995f84ee7f115e5d6e393
Gabriel Souza <gabrielpaiva423@gmail.com>
2022-07-05 11:27:33 -0300

    Fix: disbled exhaustive-deps at useEffect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -71,6 +71,7 @@ export default function Header() {
 
     useEffect(() => {
         changeLetter()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     useEffect(()=>{
         navBarReturn()
