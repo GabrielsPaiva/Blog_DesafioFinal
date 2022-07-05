129d82d518186fe6fbb2c230c9dc2f3cacc78851
Gabriel Souza <gabrielpaiva423@gmail.com>
2022-07-05 11:22:55 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import Header from './components/Header/Header';
+
+const GlobalStyle = createGlobalStyle`
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+`
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <GlobalStyle/>
+        <Header/>
+      </div>
+    );
+  }
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import media from 'styled-media-query';
+
+const Div = styled.div`
+background-color: #51659b;
+display: flex;
+justify-content: space-between;
+align-items: center;
+width: 100%;
+height: 10em;
+`
+const HeaderLogo = styled.h2`
+font-family: 'Indie Flower', 'cursive';
+font-size: 45px;
+margin-left: 16px;
+width: 45%;
+
+${media.lessThan('medium')`
+font-size: 30px;
+`}
+`
+const NavBar = styled.nav`
+display: flex;
+justify-content: space-around;
+align-items: center;
+width: 45%;
+height: 80%;
+
+${media.lessThan('medium')`
+width: 50%;
+`}
+`
+const NavItems = styled.h2`
+font-size: 25px;
+cursor: pointer;
+
+${media.lessThan('medium')`
+font-size: 20px;
+`}
+`
+
+export default function Header() {
+    const [changingLetter, setChangingLetter] = useState("o")
+
+    function changeLetter() {
+        setInterval(() => {
+            if( changingLetter === "o"){
+                setChangingLetter("a")
+            } else {
+                setChangingLetter("o")
+            }
+        }, 1000)
+    }
+
+    const navBarReturn = () => {
+        if(window.screen.availWidth <= 425){
+            return(
+                <p>hjk</p>
+            )
+        } else {
+            return(
+                <NavBar>
+                    <NavItems>Portfolio</NavItems>
+                    <NavItems>Github</NavItems>
+                    <NavItems><a href='https://gabrielsobjetivosdobairro.netlify.app'>Objetivos Locais</a></NavItems>
+                </NavBar>
+            )
+        }
+    }
+
+    useEffect(() => {
+        changeLetter()
+    }, [])
+    useEffect(()=>{
+        navBarReturn()
+    }, [changingLetter])
+
+    console.log(changingLetter)
+    return (
+        <Div>
+            <HeaderLogo>{`Bem-vind${changingLetter} ao meu blog`}</HeaderLogo>
+            {navBarReturn()}
+        </Div>
+    );
+}
